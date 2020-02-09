6ca04b54041885df5161f179117aa3f1f58bc58f
Vojta Hájek <vojtahajek@centrum.cz>
2020-02-09 02:28:32 +0100

    Build pro heroku v server.js

--- server.js
+++ server.js
@@ -61,6 +61,12 @@ app.use('/api', userRouter)
 app.use('/api', roomRouter)
 app.use('/api', messageRouter)
 
+const path = require('path')// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
+
 app.get('/checkToken', withAuth, function (req, res) {
     res.sendStatus(200)
 })
